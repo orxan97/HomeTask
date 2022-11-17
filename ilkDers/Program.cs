e6c208a40061e2a93e06a83102f0ec00b881389c
orxan97 <orkhanna@code.edu.az>
2022-11-17 17:08:54 +0400

    Add project files.

---
+++ ilkDers/Program.cs
@@ -0,0 +1,84 @@
+﻿#region task1 2 eded arasindaki ededleri ekrana yazdiran alqoritm
+//using System.Runtime.InteropServices;
+
+//int num1 =2;
+//int num2=24;
+
+//for (int i = num1; i < num2; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+#endregion
+
+#region ededin ikinin quvveti olub olmadigini gosteren alqoritm
+
+//int num = 56;
+//int count = 2;
+
+//while (count < num)
+//{
+//    count *= 2;
+//}
+//if (count == num)
+//{
+//    Console.WriteLine("2nin quvvetidir");
+
+//}
+//else
+//{
+//    Console.WriteLine("2nin quvveti deil");
+//}
+#endregion
+
+#region 3 ededden hansinin en boyuk oldugunu tapan alqoritm
+//int a = 20;
+//int b = 43;
+//int c = 35;
+
+//if (a > b && a > c)
+//{
+
+//    Console.WriteLine("en boyuk eded a-dir");
+//}
+// else if (b > a && b > c)
+//{
+
+//    Console.WriteLine("en boyuk eded b-dir");
+//}
+// else if (c > a && c > b)
+//{
+
+//    Console.WriteLine("en boyuk eded c-dir");
+//}
+
+#endregion
+
+#region iki eded arasindaki tek ededleri tapan alqoritm
+//int num = 5;
+//int num1 = 23;
+
+
+//for (int i = num; i < num1; i+=2)
+//{
+//    Console.WriteLine(i);
+//}
+#endregion
+
+#region eded 70-den boyukdurse,70 ile aralarindaki ferqin 3 qatini ekrana yazdiran,eger kicikdirse ferqin ozunu ekrana yazdiran alqoritm
+//Console.WriteLine("Zehmet olmazsa eded daxil edin");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//int num2 = 70;
+
+//if (num1 > num2)
+//{
+
+//    Console.WriteLine((num2-num1)*3);
+//}
+//else if (num1<num2)
+//{
+
+//    Console.WriteLine(num2-num1);
+//}
+#endregion
